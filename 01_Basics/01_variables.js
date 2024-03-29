f1953c178d6c98df63578ac8e1f895c27973fe12
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-03-29 08:22:10 +0000

    done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const customerId = 123
+customerCity = "Sanpada"
+let customerEmail= "harsh@gmail.com"
+var customerPass= "2122"
+
+// customerId =2 not allowed //
+
+console.log(customerId)
+
+// customerId= 456
+customerEmail= "hdfc@gmail.com"
+customerPass = "2222"
+customerCity = "vashi"
+
+console.table([customerEmail, customerPass,customerCity])
