d25ff0ec0745273864a4efb47141ade86f481c44
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-03-29 13:09:34 +0000

    everything cover

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -22,13 +22,13 @@ console.log(valueInNumber)
 
 // CONVERTING to BOOLEAN:
 
-let isLoggedIn = 1 // cam check for 0, true, 1, false, harsh
+// let isLoggedIn = 1 // cam check for 0, true, 1, false, harsh
 
 // converting to boolean
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
+// let booleanisLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 1 => true ; 0 => false
 //"" => false
@@ -37,10 +37,41 @@ console.log(booleanisLoggedIn);
 
 // string conversion
 
-let someNumber = 11
+// let someNumber = 11
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
 
 
+// ****************** Operations **********************
+
+let value = 11
+let negValue = -value
+console.log(negValue); // way of getting -ve value 
+
+console.log(2+2) // and for other operators 
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 +"2");
+// console.log((3 +4) * 5 % 3)
+// try not to write operations like above code
+
+
+// Tricky conversions:
+console.log(+true); 
+console.log(+"") // dont use operatios like this
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2 
+// dont write assignment operations like above
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+//link to study prefix and postfix:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
