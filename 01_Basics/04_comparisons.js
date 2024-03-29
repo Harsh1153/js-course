68d289690990a0f8da9cb67e7ef2b517158139ca
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-03-29 13:26:31 +0000

    comparisons in short done

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,22 @@
+console.log(2>1) // >=, <=, ==, != all the operators comparisons is easy and the output will be in T or F.
+
+// data type should be same of the values which we are comparing.
+
+
+console.log("2" >1);
+console.log("02"> 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// in the above comparisons caomparison operator convert the null to the number.
+
+// same in the case of undefined.
+// SO THIS TYPE OF ABOVE COMPARISONS SHOULD BE AVOIDED!
+
+// STRICT CHECK in this case it does not directly convert the value first it checks the data type and further proceed.(===)
+
+console.log("2" === 2);
+
+
