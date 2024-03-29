a16b3328298bc3c8f3457f3988fa54339181918d
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-03-29 10:20:51 +0000

    done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as new newer version 
+
+// alert(3 + 3) // it is run in browser , we are using nodejs not browser
+
+console.log(3+ 3)
+
+console.log("harsh")
+
+let name= "harsh"
+let age = 18
+let isLoggedIn = false //boolean datatype
+
+/*
+number => 2 to the power 53
+bigint
+string => "" try to use double quotes
+boolean => T or F
+null => standalone value
+undefined => let state; this is undefined value that is value is not assigned yet
+symbol => unique
+
+object => we can also called null as object 
+*/
+
+console.log(typeof "harsh") // string
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+
