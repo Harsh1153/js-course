1dbe7260200713820d232542163ffe38972dd8cb
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-03-30 05:23:24 +0000

    done

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,5 +1,8 @@
-console.log("harsh")
+// console.log("harsh")
 
-
+let a = [];
+console.log( a++ )
+// Before console.log(a++), a is an empty array [].
+// a++ increments a by 1 (which doesn't affect an empty array), returns the original value of a before the increment (which is 0 after conversion from an empty array), and prints that value (0) to the console.
 
 
