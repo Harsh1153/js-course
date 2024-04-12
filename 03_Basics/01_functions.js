c100e773ed44c4a97f6573e6cab09884c67a78e9
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-04-12 03:42:29 +0000

    functions done

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -53,3 +53,41 @@ console.log(loginUserMessage()); // undefined, just logged in - will be the outp
 // we can also give the default values for the defined function and while executing the function if we give any other value then it will override that default value
 // function loginUserMessage(username= "harsh")
 
+// "function ke naam humesha aise dena jisko padhke samje ki function kya kar rah hai"
+
+// to add multiple values in the cart and add them in one array or keep them together we use "rest or spread operator"
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,300,400,2000,5000));
+// the val1 and val2 will get the first two values of the cart and rest all the items will be printed
+// It gives output for the above code and this is what the use of rest operator - [ 200, 300, 400, 2000, 5000 ]
+
+
+// Object ko function me kaise pass karte hai and uska use :-
+const user = {
+    userName: "Harsh",
+    price: 1000
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+
+//handleObject(user) // function ko call karke and object pass kiya which is necessary
+// we can also directly pass the object while function executing.
+handleObject({
+    userName: "Aadi",
+    price: 1000
+})
+// "type safety check karna imp hai jo bhi value pass kar rahe hai uska data type object hai ki nahi and also jo bhi elements extract kar rahe hai voh object me avail hai kya which is also imp to check for that purpose typeScript  is imp!"
+
+// Now will check how we define function to get the value of array and execute it :-
+const myArray = [100,200,300,400]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+console.log(returnSecondValue(myArray));
+// we can also directly declare array while function executing:-
+console.log(returnSecondValue([100,200,400,500]));
