db39fc928cbae37a141740727b48274ea948cec1
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-06-29 14:17:12 +0000

    done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// SWITCH case will be useful further in REDUX.
+// Basic Syntax of Switch case are as follows:-
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// EXAMPLE to find Month:-
+const month = 3
+switch (month) {
+    case 1:
+        console.log(`january`);
+        break;
+    case 2:
+        console.log(`february`);
+        break;
+    case 3:
+        console.log(`march`);
+        break;
+    case 4:
+        console.log(`march`);
+        break;
+
+    default:
+        console.log(`default case match!`);
+        break;
+}
+
