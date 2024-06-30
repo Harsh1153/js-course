4955edd27d3332d6e4ec3aa06c41822e48bbfc70
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-06-30 15:01:29 +0000

    done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -11,9 +11,29 @@ for (let i = 0; i <=10; i++) {
 
 // for loop inside for loop:-
 for (let i = 1; i <=10; i++) {
-    console.log(`Outer loop value: ${i}`); 
+    //console.log(`Outer loop value: ${i}`); 
     for (let j = 1; j <=10; j++) {
     //console.log(`Inner loop value ${j} and inner loop ${i}`);
-    console.log(i + '*' + j + '=' + i*j);        
+    //console.log(i + '*' + j + '=' + i*j);        
     }   
+}
+
+// As this for loop also works in the case of array:-
+const myArray = ["batman", "superman", "spiderman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+//console.log(myArray.length);
+
+// Keywords used in loops:- 1] break & 2] continue
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`5 Detected`);
+        //break
+        // continue will skip the condition one time
+        continue
+    }
+    console.log(`value of i: ${index}`);  // here we printed 20 numbers using loop, but what if any condition occurs that is 5 num is best  and afterwards dont print any number. The condition will come before the log statement.  
 }
