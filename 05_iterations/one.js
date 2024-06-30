eac87bdd36064e79b870798e12c6a76b79c42641
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-06-30 09:47:13 +0000

    done

---
+++ 05_iterations/one.js
@@ -0,0 +1,10 @@
+// for loop:-
+for (let i = 0; i <=10; i++) {
+    const element = i; 
+    // to change the name of variables use ctr + d.
+    if(element == 10){
+        //console.log(`10 is the last number`);
+    }
+    //console.log(element);
+}
+// the above was the example of for loop and loops inside loop.
