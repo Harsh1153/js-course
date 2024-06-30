1cbf04740c1757d3329dfa8f3de4f72d2219ad2f
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-06-30 09:54:07 +0000

    done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -8,3 +8,12 @@ for (let i = 0; i <=10; i++) {
     //console.log(element);
 }
 // the above was the example of for loop and loops inside loop.
+
+// for loop inside for loop:-
+for (let i = 1; i <=10; i++) {
+    console.log(`Outer loop value: ${i}`); 
+    for (let j = 1; j <=10; j++) {
+    //console.log(`Inner loop value ${j} and inner loop ${i}`);
+    console.log(i + '*' + j + '=' + i*j);        
+    }   
+}
