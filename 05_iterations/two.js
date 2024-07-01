8579d06b756e997030b56d26a9074530acde8f21
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-07-01 08:05:10 +0000

    done

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// lets see While & do-while loop in js:-
+let index = 0
+while (index <= 10) {
+   // console.log(`value of i is ${index}`);
+    index = index + 2
+}
+// same for array:-
+const myArray = ['flash', 'superman', 'batman']
+let arr=0
+while (arr<myArray.length) {
+   // console.log(`${myArray[arr]}`);
+    arr = arr + 1
+}
+// it doesnt matter you print the value using for loop , while loop or do-while loop.
+
+// do-while loop:-
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
