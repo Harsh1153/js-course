94ff7b27d5de78516f35a0d846a14915cda838ae
Harsh Rajendra Shirole <130602296+Harsh1153@users.noreply.github.com>
2024-07-01 08:12:23 +0000

    in progress

---
+++ 05_iterations/three.js
@@ -0,0 +1,6 @@
+// High Order Loops in Array:-these all are array specific loops.
+
+// [{},{}] :- yaha pr objects ke upar loop lagana matlb kis chiz pr lagana hai loop
+
+//lets see for of loop:-
+const arr = [1,2,3,4,5]
